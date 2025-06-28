1b55bf9ae6bed3c3b8b391b81c882cbfc016f653
Peter Výboch <pvyboch1@gmail.com>
2025-06-28 12:25:05 +0200

    feat: exposing fixMesh function

--- src/MeshFix/meshfix.cpp
+++ src/MeshFix/meshfix.cpp
@@ -127,86 +127,89 @@ char *createFilename(const char *iname, const char *subext, char *oname, const c
 	return oname;
 }
 
+extern "C" int fixMesh(
+    const char* infilename,
+    const char* outfilename,
+    bool join_multiple_components,
+    bool stl_output,
+    bool skip_if_fixed
+) {
+    TMesh::init();
+    TMesh::app_name = "MeshFix";
+    TMesh::app_version = "2.0";
+    TMesh::app_year = "2016";
+    TMesh::app_authors = "Marco Attene";
+    TMesh::app_maillist = "attene@ge.imati.cnr.it";
+
+    clock_t beginning = clock();
+    char extension[16];
+    strcpy(extension, stl_output ? ".stl" : ".off");
+    char outfilename_buf[2048];
+    if (!outfilename || strlen(outfilename) == 0) {
+        createFilename(infilename, "_fixed", outfilename_buf, extension);
+        outfilename = outfilename_buf;
+    }
+
+    if (skip_if_fixed && fopen(outfilename, "r")) {
+        TMesh::error("Output file already exists (-x option specified).");
+        return 2;
+    }
+
+    Basic_TMesh tin;
+    if (tin.load(infilename) != 0) {
+        TMesh::error("Can't open file.\n");
+        return 1;
+    }
+
+    if (join_multiple_components) {
+        TMesh::info("\nJoining input components ...\n");
+        TMesh::begin_progress();
+        while (joinClosestComponents(&tin)) TMesh::report_progress("Num. components: %d       ", tin.shells());
+        TMesh::end_progress();
+        tin.deselectTriangles();
+    }
+
+    int sc = tin.removeSmallestComponents();
+    if (sc) TMesh::warning("Removed %d small components\n", sc);
+
+    if (tin.boundaries()) {
+        TMesh::warning("Patching holes\n");
+        tin.fillSmallBoundaries(0, true);
+    }
+
+    if (!tin.boundaries()) TMesh::warning("Fixing degeneracies and intersections...\n");
+    if (tin.boundaries() || !tin.meshclean()) TMesh::warning("MeshFix could not fix everything.\n", sc);
+
+    TMesh::info("Saving output mesh ...\n");
+    tin.save(outfilename);
+
+    if (CLOCKS_PER_SEC >= 1000)
+        printf("Elapsed time: %d ms\n", (clock() - beginning) / (CLOCKS_PER_SEC / 1000));
+
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
- TMesh::init(); // This is mandatory
- TMesh::app_name = "MeshFix";
- TMesh::app_version = "2.0";
- TMesh::app_year = "2016";
- TMesh::app_authors = "Marco Attene";
- TMesh::app_maillist = "attene@ge.imati.cnr.it";
-
- clock_t beginning = clock();
-
- // Uncomment the following to prevent message reporting
- // TMesh::quiet = true;
-
- Basic_TMesh tin;
- bool stl_output = false;
- bool skip_if_fixed = false;
- bool join_multiple_components = false;
- char infilename[2048], outfilename[2048], extension[] = ".off";
-
- if (argc < 2) usage();
-
- float par;
- int i = 2;
- if (argc > 2 && argv[2][0] == '-') i--;
-
- for (; i<argc; i++)
- {
-  if (i<argc-1) par = (float)atof(argv[i+1]); else par = 0;
-  if (!strcmp(argv[i], "-x")) skip_if_fixed = true;
-  else if (!strcmp(argv[i], "-a")) join_multiple_components = true;
-  else if (!strcmp(argv[i], "-j")) stl_output = true;
-  else if (argv[i][0] == '-') TMesh::warning("%s - Unknown operation.\n", argv[i]);
-
-  if (par) i++;
- }
-
- sprintf(infilename, "%s", argv[1]);
- if (stl_output) strcpy(extension, ".stl");
- if (argc>2 && argv[2][0] != '-') sprintf(outfilename, "%s", argv[2]);
- else createFilename(infilename, "_fixed", outfilename, extension);
-
- if (skip_if_fixed && fopen(outfilename, "r")) TMesh::error("Output file already exists (-x option specified).");
-
- // The loader automatically reconstructs a manifold triangle connectivity
- if (tin.load(infilename) != 0) TMesh::error("Can't open file.\n");
-
- if (join_multiple_components)
- {
-	 TMesh::info("\nJoining input components ...\n");
-	 TMesh::begin_progress();
-	 while (joinClosestComponents(&tin)) TMesh::report_progress("Num. components: %d       ", tin.shells());
-	 TMesh::end_progress();
-	 tin.deselectTriangles();
- }
-
-	   // Keep only the largest component (i.e. with most triangles)
-	   int sc = tin.removeSmallestComponents();
-	   if (sc) TMesh::warning("Removed %d small components\n",sc);
-
-	   // Fill holes
-	   if (tin.boundaries())
-	   {
-		TMesh::warning("Patching holes\n");
-		tin.fillSmallBoundaries(0, true);
-	   }
-
-	   // Run geometry correction
-	   if (!tin.boundaries()) TMesh::warning("Fixing degeneracies and intersections...\n");
-	   if (tin.boundaries() || !tin.meshclean()) TMesh::warning("MeshFix could not fix everything.\n", sc);
-
-
- TMesh::info("Saving output mesh ...\n");
- tin.save(outfilename);
-
- // Normally, CLOCKS_PER_SEC >= 1000, but this is not technically guaranteed,
- // and if it were not true, dividing by zero would invoke undefined behavior.
- if (CLOCKS_PER_SEC >= 1000)
-	 printf("Elapsed time: %d ms\n", (clock() - beginning) / (CLOCKS_PER_SEC / 1000));
-
- return 0;
+    if (argc < 2) usage();
+    char infilename[2048], outfilename[2048], extension[] = ".off";
+    bool stl_output = false;
+    bool skip_if_fixed = false;
+    bool join_multiple_components = false;
+    float par;
+    int i = 2;
+    if (argc > 2 && argv[2][0] == '-') i--;
+    for (; i<argc; i++) {
+        if (i<argc-1) par = (float)atof(argv[i+1]); else par = 0;
+        if (!strcmp(argv[i], "-x")) skip_if_fixed = true;
+        else if (!strcmp(argv[i], "-a")) join_multiple_components = true;
+        else if (!strcmp(argv[i], "-j")) stl_output = true;
+        else if (argv[i][0] == '-') TMesh::warning("%s - Unknown operation.\n", argv[i]);
+        if (par) i++;
+    }
+    sprintf(infilename, "%s", argv[1]);
+    if (stl_output) strcpy(extension, ".stl");
+    if (argc>2 && argv[2][0] != '-') sprintf(outfilename, "%s", argv[2]);
+    else createFilename(infilename, "_fixed", outfilename, extension);
+    return fixMesh(infilename, outfilename, join_multiple_components, stl_output, skip_if_fixed);
 }
